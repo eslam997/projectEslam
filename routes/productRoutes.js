758107c48d0657c4e38282df38c20339bc6de2f6
eslam997 <e.saad19081@gmail.com>
2025-05-13 01:29:01 +0300

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,11 +4,13 @@ const {
   getProducts,
   getSingleProduct,
   addProduct,
+  updateProduct,
 } = require("../controllers/product");
 
 
 routerProducts.get("/", getProducts);
 routerProducts.get("/:id", getSingleProduct);
+routerProducts.put("/:id", updateProduct);
 routerProducts.post("/", addProduct);
 
 module.exports = routerProducts;
